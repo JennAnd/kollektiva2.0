a4b4aeaffe8c45b7c102c5cc4fcbee214c662551
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-03-30 13:18:29 +0200

    components
    prepared components

---
+++ src/components/Button/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./button.css";
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick} className="btn">
+      {props.buttonText}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  buttonText: PropTypes.string,
+  handleClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  buttonText: "what?",
+};
+
+export default Button;
