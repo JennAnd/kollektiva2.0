92cc4c57d5de70e70e44211551b3878de4dfb159
h-emma <emmah92@hotmail.com>
2022-04-04 21:01:00 +0200

    Add structure for the description page

--- src/pages/Description/index.js
+++ src/pages/Description/index.js
@@ -1,7 +1,32 @@
 import React from "react";
-import "./App.css";
-import Button from "./components/Button";
+import { navigate } from "@reach/router";
+import "./description.css";
+import Button from "../../components/Button";
 
-const DescriptionPage = (props) => {};
+const DescriptionPage = (props) => {
+  return (
+    <div class="descriptionChoice">
+      <h1>Ge en beskrivning</h1>
+      <p>Ge en generell beskrivning av bostaden och dess område.</p>
+      <form>
+        <textarea cols="50" rows="20" />
+      </form>
+
+      {/*  <div class="helpButton">
+        <Button buttonText="Hjälp" bg="white" border="2px solid #000000" />
+      </div> */}
+      <div class="pageNavigation">
+        <Button
+          handleClick={() => navigate("/outside")}
+          buttonText="Tillbaka"
+        />
+        <Button
+          handleClick={() => navigate("/advertisement-view")}
+          buttonText="Spara & fortsätt"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default DescriptionPage;
