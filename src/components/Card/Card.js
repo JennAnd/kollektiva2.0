fffe862a7eb4715a1ef6051725f6f6d91177071d
h-emma <emmah92@hotmail.com>
2022-04-07 21:32:38 +0200

    Change structure on Button component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,14 +3,14 @@ import PropTypes from "prop-types";
 import styles from "./Card.module.css";
 import Button from "../Button/Button";
 
-const Card = ({ image, title, text }) => {
+const Card = ({ image, title, text, buttonText, bg }) => {
   return (
     <article className={styles.card}>
       <img src={image} alt="houseroof" />
       <div className={styles.cardContent}>
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.text}>{text}</p>
-        <Button buttonText="Läs mer om att hyra ut din bostad" bg="white" />
+        <Button className={styles.btn}>{buttonText}</Button>
       </div>
     </article>
   );
@@ -20,5 +20,7 @@ Card.propTypes = {
   image: PropTypes.string,
   title: PropTypes.string,
   text: PropTypes.string,
+  buttonText: PropTypes.string,
+  bg: PropTypes.string,
 };
 export default Card;
