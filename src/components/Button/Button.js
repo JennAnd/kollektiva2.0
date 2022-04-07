6368b853669088a76bfeae470a2d2cb015a50131
h-emma <emmah92@hotmail.com>
2022-04-07 21:32:15 +0200

    Change structure on Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,40 +1,17 @@
 import React from "react";
-import styles from "./Button.module.css";
 import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 
-const Button = (props) => {
+const Button = ({ handleClick, children }) => {
   return (
-    <button
-      onClick={props.handleClick}
-      className={styles.btn}
-      style={{
-        backgroundColor: props.bg,
-        border: props.border,
-        color: props.color,
-        icon: props.icon,
-
-        /* width: props.width, */
-      }}
-    >
-      {props.buttonText}
+    <button onClick={handleClick} className={styles.btn}>
+      {children}
     </button>
   );
 };
 
-/* const btnHelp = (props) => <Button {...props} />; */
-
 Button.propTypes = {
-  buttonText: PropTypes.string,
-  bg: PropTypes.string,
   handleClick: PropTypes.func,
-  border: PropTypes.string,
-  color: PropTypes.string,
-  icon: PropTypes.string,
-  /* width:PropTypes.string, */
-};
-
-Button.defaultProps = {
-  buttonText: "error",
 };
 
 export default Button;
