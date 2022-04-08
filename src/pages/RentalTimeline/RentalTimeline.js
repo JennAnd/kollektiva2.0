3217b14d18a17501311c771c7198d0003efd9354
h-emma <emmah92@hotmail.com>
2022-04-08 21:15:31 +0200

    WIP

--- src/pages/RentalTimeline/RentalTimeline.js
+++ src/pages/RentalTimeline/RentalTimeline.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
 import { navigate } from "@reach/router";
 import styles from "./RentalTimeline.module.css";
 import Button from "../../components/Button/Button";
 import Footer from "../../components/Footer/Footer";
+import "react-datepicker/dist/react-datepicker.css";
 
-const RentalTimelinePage = (props) => {
+const RentalTimelinePage = () => {
+  const [startDate, setStartDate] = useState(new Date("2022/01/01"));
+  const [endDate, setEndDate] = useState(new Date("2022/01/02"));
+
   return (
     <div className={styles.choice}>
       <h1 className={styles.title}>Tidsperiod</h1>
       <p className={styles.text}>Välj hur länge du vill hyra ut din bostad.</p>
-
+      <div className={styles.datePickerContainerStart}>
+        <p className={styles.textDatePicker}>Från</p>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+        />
+      </div>
+      <div className={styles.datePickerContainerEnd}>
+        <p className={styles.textDatePicker}>Till</p>
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+        />
+      </div>
       <div className={styles.pageNavigation}>
         <Button
           handleClick={() => navigate("/number-of-people")}
