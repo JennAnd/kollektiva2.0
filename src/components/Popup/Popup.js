2c9fb73820f80ea6cb1ca6d1de3c43574fec2687
h-emma <emmah92@hotmail.com>
2022-04-10 20:01:44 +0200

    Add a popup component

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Popup.module.css";
+
+function Popup(props) {
+  return props.trigger ? (
+    <div className={styles.popup}>
+      <div className={styles.popupInner}>
+        <button
+          className={styles.closeBtn}
+          onClick={() => props.setTrigger(false)}
+        >
+          X
+        </button>
+        {props.children}
+      </div>
+    </div>
+  ) : (
+    ""
+  );
+}
+
+export default Popup;
