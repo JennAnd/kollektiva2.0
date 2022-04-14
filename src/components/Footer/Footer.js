cb051544b4c969ca7c7a97471caf2eeb79e55b5f
h-emma <emmah92@hotmail.com>
2022-04-14 22:01:12 +0200

    Add div with classname

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,83 +11,99 @@ const Footer = () => {
     <footer>
       <div className={styles.contactUs}>
         <h2 className={styles.companyName}>Kollektiva</h2>
-        <h3 className={styles.subheading}>Kontakta oss</h3>
-        <p className={styles.text}>
-          Kontakta oss vid behov av hjälp med registering, annonsering och
-          övrigt
-        </p>
-        <div className={styles.link}>
-          <img src={phone} alt="phone icon" />
-          <a className={styles.contacts} href="tel:010-70 75 100">
-            010-70 75 100
-          </a>
+        <div className={styles.contactUsText}>
+          <h3 className={styles.subheading}>Kontakta oss</h3>
+          <p className={styles.text}>
+            Kontakta oss vid behov av hjälp med registering, annonsering och
+            övrigt
+          </p>
         </div>
+        <div className={styles.contactLinks}>
+          <div className={styles.link}>
+            <img src={phone} alt="phone icon" />
+            <a className={styles.contacts} href="tel:010-70 75 100">
+              010-70 75 100
+            </a>
+          </div>
 
-        <div className={styles.link}>
-          <img src={mail} alt="mail icon" />
-          <a className={styles.contacts} href="mailto:webmaster@example.com">
-            support@kollektiva.se
-          </a>
+          <div className={styles.link}>
+            <img src={mail} alt="mail icon" />
+            <a className={styles.contacts} href="mailto:webmaster@example.com">
+              support@kollektiva.se
+            </a>
+          </div>
         </div>
       </div>
       <div className={styles.linkPage}>
-        <h3 className={styles.subheadingFirst}>Hyr ut bostad</h3>
-        <a href="/" className={styles.textInfo}>
-          Frågor och svar
-        </a>
-        <a href="/" className={styles.textInfo}>
-          Berättelser
-        </a>
-        <a href="/" className={styles.textInfo}>
-          Hur det fungerar
-        </a>
-        <h3 className={styles.subheading}>Hyra bostad</h3>
-        <a href="/" className={styles.textInfo}>
-          Frågor och svar{" "}
-        </a>
-        <a href="/" className={styles.textInfo}>
-          Annonser
-        </a>
-        <h3 className={styles.subheading}>Kollektiva</h3>
-        <a href="/" className={styles.textInfo}>
-          Om oss
-        </a>
-        <a href="/" className={styles.textInfo}>
-          Allmänna villkor{" "}
-        </a>
-        <h3 className={styles.subheading}>Stöd</h3>
-        <a href="/" className={styles.textInfo}>
-          Hjälp och guide
-        </a>
-        <a href="/" className={styles.textInfo}>
-          Tillgänglighet
-        </a>
-        <a href="/" className={styles.textInfoLast}>
-          {/* Hjälp och guide{" "} */}Säkerhetsinformation
-        </a>
+        <div className={styles.rentOut}>
+          <h3 className={styles.subheadingFirst}>Hyr ut bostad</h3>
+          <a href="/" className={styles.textInfo}>
+            Frågor och svar
+          </a>
+          <a href="/" className={styles.textInfo}>
+            Berättelser
+          </a>
+          <a href="/" className={styles.textInfo}>
+            Hur det fungerar
+          </a>
+        </div>
+        <div className={styles.rent}>
+          <h3 className={styles.subheading}>Hyra bostad</h3>
+          <a href="/" className={styles.textInfo}>
+            Frågor och svar{" "}
+          </a>
+          <a href="/" className={styles.textInfo}>
+            Annonser
+          </a>
+        </div>
+        <div className={styles.aboutUs}>
+          <h3 className={styles.subheading}>Kollektiva</h3>
+          <a href="/" className={styles.textInfo}>
+            Om oss
+          </a>
+          <a href="/" className={styles.textInfo}>
+            Allmänna villkor{" "}
+          </a>
+        </div>
+        <div className={styles.support}>
+          <h3 className={styles.subheading}>Stöd</h3>
+          <a href="/" className={styles.textInfo}>
+            Hjälp och guide
+          </a>
+          <a href="/" className={styles.textInfo}>
+            Tillgänglighet
+          </a>
+          <a href="/" className={styles.textInfoLast}>
+            {/* Hjälp och guide{" "} */}Säkerhetsinformation
+          </a>
+        </div>
       </div>
       <div className={styles.socialMedia}>
-        <h3 className={styles.subheadingFirst}>Sociala medier</h3>
-        <p className={styles.text}>
-          Följ oss för nyheter och uppdateringar om Kollektiva.
-        </p>
-        <div className={styles.link}>
-          <img src={facebook} alt="facebook icon" />
-          <a href="/" className={styles.textInfo}>
-            Facebook
-          </a>
+        <div className={styles.socialMediaText}>
+          <h3 className={styles.subheadingFirst}>Sociala medier</h3>
+          <p className={styles.text}>
+            Följ oss för nyheter och uppdateringar om Kollektiva.
+          </p>
         </div>
-        <div className={styles.link}>
-          <img src={twitter} alt="twitter icon" />
-          <a href="/" className={styles.textInfo}>
-            Twitter
-          </a>
-        </div>
-        <div className={styles.link}>
-          <img src={instagram} alt="instagram icon" />
-          <a href="/" className={styles.textInfo}>
-            Instagram
-          </a>
+        <div className={styles.socialMediaLinks}>
+          <div className={styles.link}>
+            <img src={facebook} alt="facebook icon" />
+            <a href="/" className={styles.textInfo}>
+              Facebook
+            </a>
+          </div>
+          <div className={styles.link}>
+            <img src={twitter} alt="twitter icon" />
+            <a href="/" className={styles.textInfo}>
+              Twitter
+            </a>
+          </div>
+          <div className={styles.link}>
+            <img src={instagram} alt="instagram icon" />
+            <a href="/" className={styles.textInfo}>
+              Instagram
+            </a>
+          </div>
         </div>
       </div>
       <p className={styles.copyright}>Copyright 2022 © Kollektiva</p>
