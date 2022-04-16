60e2e49a1452b76a30cb032e7ba79a354c3c0fd1
h-emma <emmah92@hotmail.com>
2022-04-16 07:22:41 +0200

    Add onClick with navigation on button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,13 +3,15 @@ import PropTypes from "prop-types";
 import styles from "./Card.module.css";
 import Button from "../Button/Button";
 
-const Card = ({ title, text, buttonText, bg }) => {
+const Card = ({ title, text, buttonText, onClick }) => {
   return (
     <article className={styles.card}>
       <div className={styles.cardContent}>
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.text}>{text}</p>
-        <button className={styles.btn}>{buttonText}</button>
+        <button className={styles.btn} onClick={onClick}>
+          {buttonText}
+        </button>
       </div>
     </article>
   );
@@ -19,5 +21,6 @@ Card.propTypes = {
   title: PropTypes.string,
   text: PropTypes.string,
   buttonText: PropTypes.string,
+  onClick: PropTypes.func,
 };
 export default Card;
